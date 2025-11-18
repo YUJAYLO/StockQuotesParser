805f9f7f69056edacc0212ac04f36feda8d46dc6
YUJAYLO <evaairb797@gmail.com>
2025-11-18 21:33:31 +0800

    finished: format

--- include/format.h
+++ include/format.h
@@ -8,7 +8,7 @@
 namespace Format {
 
     const uint8_t ESC_CODE = 0x1B;
-    const uint8_t TERMINAL_CODE[2] = {0x0D, 0x0A};
+    const uint16_t TERMINAL_CODE = 0x0D0A;
 
     #pragma pack(push, 1)
     struct HEADER {
@@ -16,7 +16,23 @@ namespace Format {
         uint8_t businessType;
         uint8_t formatCode;
         uint8_t version;
-        char reserved[5];
+        uint32_t serialNuum;
+    };
+    #pragma pack(pop)
+
+    #pragma pack(push, 1)
+    template<int N>
+    struct RawRecord {
+        // ESC (1)
+        uint8_t esc;
+        // HEADER (9)
+        HEADER header;
+        // BODY (N)
+        uint8_t body[N];
+        // checksum (1)
+        uint8_t checksum;
+        // terminal (2)
+        uint16_t terminal;
     };
     #pragma pack(pop)
 
@@ -25,13 +41,38 @@ namespace Format {
         struct ParsedRecord {
             std::string symbolCode;
             std::string symbolShort;
-            double refPrice;
-            double upLimit;
-            double downLimit;
+            std::string refPrice;
+            std::string upLimit;
+            std::string downLimit;
             std::string timestamp;
         };
 
-        void process(const std::vector<uint8_t>& record);
+    private:
+        #pragma pack(push, 1)
+        struct BODY {
+            char symbolCode[6];
+            char symbolShort[16];
+            uint8_t stockDataReserved1[8];
+            uint8_t refPrice[5];
+            uint8_t upLimit[5];
+            uint8_t downLimit[5];
+            uint8_t stockDataReserved2[9];
+            uint8_t warrantDataReserved[39];
+            uint8_t otherDataReserved[7];
+            uint8_t routeMark;
+        };
+        #pragma pack(pop)
+
+        static constexpr size_t BODY_SIZE = 101;
+
+        uint16_t _messageLength = 0x0114;
+        uint8_t _businessType = 0x01;
+        uint8_t _version = 0x09;
+        BODY _body;
+
+    public:
+        Format1(const std::vector<uint8_t>& record);
+        void process();
     };
 
 } // namespace Format
--- src/format.cpp
+++ src/format.cpp
@@ -1,26 +1,32 @@
 #include "..\include\format.h"
 #include "..\include\dataAccess.h"
-#include "..\include\converter.h"
+#include "..\include\utils\converter.h"
 #include <iostream>
 #include <cstring>
 #include <ctime>
 
 namespace Format {
 
-void Format1::process(const std::vector<uint8_t>& record) {
-    const HEADER* header = reinterpret_cast<const HEADER*>(&record[1]);
-
-    if (header->messageLength != 114 || header->businessType != 0x01 ||
-        header->formatCode != 0x01 || header->version != 0x09) {
+Format1::Format1(const std::vector<uint8_t>& record) {
+    const RawRecord<BODY_SIZE>* rawRecord = reinterpret_cast<const RawRecord<BODY_SIZE>*>(record.data());
+    // std::memcpy(&rawRecord, record.data(), sizeof(RawRecord<BODY_SIZE>));
+    
+    if( rawRecord->header.messageLength != _messageLength ||
+        rawRecord->header.businessType != _businessType ||
+        rawRecord->header.version != _version) {
         std::cerr << "Warning: Unexpected HEADER values." << std::endl;
     }
+    std::memcpy(&_body, rawRecord->body, sizeof(BODY));
+}
+
+void Format1::process() {
 
     ParsedRecord parsed;
-    parsed.symbolCode = std::string(reinterpret_cast<const char*>(&record[20]), 6);
-    parsed.symbolShort = Converter::Big5ToUTF8(std::string(reinterpret_cast<const char*>(&record[10]), 10));
-    parsed.refPrice = Converter::packBcdDecode(&record[26], 5, 5, 4);
-    parsed.upLimit = Converter::packBcdDecode(&record[31], 5, 5, 4);
-    parsed.downLimit = Converter::packBcdDecode(&record[36], 5, 5, 4);
+    parsed.symbolCode = std::string(_body.symbolCode, 6);
+    parsed.symbolShort = Converter::Big5ToUTF8(std::string(_body.symbolShort, 16));
+    parsed.refPrice = Converter::PACKBCDToString(_body.refPrice, 5, 5, 4);
+    parsed.upLimit = Converter::PACKBCDToString(_body.upLimit, 5, 5, 4);
+    parsed.downLimit = Converter::PACKBCDToString(_body.downLimit, 5, 5, 4);
 
     // Get current timestamp
     std::time_t t = std::time(nullptr);
